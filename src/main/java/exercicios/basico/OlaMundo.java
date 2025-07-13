bba426a2abedf94590b5fc8cb32f219864b5c6e4
marcosf7x <marcos.felipe1088@gmail.com>
2025-07-12 23:35:12 -0300

    Initial commit (Add project e codes)

---
+++ src/main/java/exercicios/basico/OlaMundo.java
@@ -0,0 +1,7 @@
+package exercicios.basico;
+
+public class OlaMundo {
+    public static void main (String [] args){
+        System.out.println("Olá, mundo. Felipe, força e honra. Continue na pegada!");
+    }
+}
