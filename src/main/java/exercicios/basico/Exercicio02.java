917bb61adf1086ae455166e8e58d784b5bf7d3f4
marcosf7x <marcos.felipe1088@gmail.com>
2025-07-14 23:32:36 -0300

    Add new exercise (Exercicio2)

---
+++ src/main/java/exercicios/basico/Exercicio02.java
@@ -0,0 +1,17 @@
+package exercicios.basico;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá, " + nome + "! Bem-vindo ao Exercicio 02.");
+
+        scanner.close();
+    }
+}
+
