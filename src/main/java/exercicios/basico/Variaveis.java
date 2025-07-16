860d6af83da2277bc3f4a54fc180da56e85b172d
marcosf7x <marcos.felipe1088@gmail.com>
2025-07-15 23:55:03 -0300

    Add new exercise (Variaveis)

---
+++ src/main/java/exercicios/basico/Variaveis.java
@@ -0,0 +1,25 @@
+package exercicios.basico;
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+
+        int idade = 20;
+        String nome = "Felipe";
+        String nomeDoMeuCachorro = "Totó";
+
+        int _idade;
+        int $idade;
+        String ano2014;
+
+        String nome_do_meu_cachorro;
+        String NomeDoMeuCachorro;
+        String nomeDoMeucachorro;
+
+        idade = 25;
+
+        System.out.println("Idade = " + idade);
+
+    }
+
+}
